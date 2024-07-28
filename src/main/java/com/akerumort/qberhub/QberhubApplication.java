0eea58ec0f022c34ba756f3bfa88d680a4a2b721
akerumort <akerumort@gmail.com>
2024-07-28 16:32:26 +0300

    added Spring Boot project

---
+++ src/main/java/com/akerumort/qberhub/QberhubApplication.java
@@ -0,0 +1,13 @@
+package com.akerumort.qberhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QberhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QberhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akerumort/qberhub/QberhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akerumort.qberhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QberhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
