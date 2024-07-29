a7a412f1d405b233346d9f737653c211d279607a
akerumort <akerumort@gmail.com>
2024-07-29 15:23:47 +0300

    added entity

---
+++ user-service/src/main/java/com/akerumort/userservice/entities/User.java
@@ -0,0 +1,35 @@
+package com.akerumort.userservice.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(max = 255, message = "Username can't be longer than 255 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 255, message = "Email can't be longer than 255 characters")
+    private String email;
+
+    @Size(max = 1024, message = "Bio can't be longer than 1024 characters")
+    private String bio;
+}
