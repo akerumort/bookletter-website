08a8731cafde18d7093bc429515b2b3f70f05e32
akerumort <akerumort@gmail.com>
2024-07-29 16:17:26 +0300

    added controller for crud operations with pagination

---
+++ user-service/src/main/java/com/akerumort/userservice/controllers/UserController.java
@@ -0,0 +1,72 @@
+package com.akerumort.userservice.controllers;
+
+import com.akerumort.userservice.dto.UserCreateDTO;
+import com.akerumort.userservice.dto.UserDTO;
+import com.akerumort.userservice.entities.User;
+import com.akerumort.userservice.mappers.UserMapper;
+import com.akerumort.userservice.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping
+    public List<UserDTO> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        List<User> users = userService.getAllUsers(page, size);
+        return users.stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(
+            @PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return userMapper.toDTO(user);
+    }
+
+    @PostMapping
+    public UserDTO createUser(
+            @Valid @RequestBody UserCreateDTO userCreateDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new RuntimeException(bindingResult.getAllErrors().toString());
+        }
+        User user = userMapper.toEntity(userCreateDTO);
+        User savedUser = userService.saveUser(user);
+        return userMapper.toDTO(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(
+            @PathVariable Long id,
+            @Valid @RequestBody UserCreateDTO userCreateDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new RuntimeException(bindingResult.getAllErrors().toString());
+        }
+        User user = userMapper.toEntity(userCreateDTO);
+        user.setId(id);
+        User updatedUser = userService.saveUser(user);
+        return userMapper.toDTO(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(
+            @PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
