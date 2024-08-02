e0768ec7850f33333099dd828c8fe965edeb879f
akerumort <akerumort@gmail.com>
2024-08-02 18:01:15 +0300

    added 'SecurityConfig', registration and authentication now work

---
+++ user-service/src/main/java/com/akerumort/userservice/configs/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.akerumort.userservice.configs;
+
+import com.akerumort.userservice.filters.JwtAuthenticationFilter;
+import com.akerumort.userservice.services.CustomUserDetailsService;
+import com.akerumort.userservice.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/users/register").permitAll()
+                .requestMatchers("/users/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userDetailsService()), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+}
