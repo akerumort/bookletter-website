ac28b479668878b5c4ad3165d266666041f07ec4
Ivan Rodionov <144949516+akerumort@users.noreply.github.com>
2024-09-29 18:40:29 +0300

    fixed view count in mapper

--- post-service/src/main/java/com/akerumort/postservice/mappers/PostMapper.java
+++ post-service/src/main/java/com/akerumort/postservice/mappers/PostMapper.java
@@ -4,10 +4,12 @@ import com.akerumort.postservice.dto.PostCreateDto;
 import com.akerumort.postservice.dto.PostResponseDto;
 import com.akerumort.postservice.entities.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
     Post toEntity(PostCreateDto dto);
 
+    @Mapping(target = "viewCount", source = "entity.viewCount")
     PostResponseDto toDto(Post entity);
 }
